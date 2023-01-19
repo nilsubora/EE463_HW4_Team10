5dcc6bea2593a91f179e7b306f50bfd8988ba0ef
nilsubora <115696064+nilsubora@users.noreply.github.com>
2023-01-19 23:02:13 +0300

    q2 updates
    parts a b c

--- hw4q2equations.m
+++ hw4q2equations.m
@@ -1,37 +1,35 @@
 clc
-
-Vd = [ 5 6 7 8 9 10 11 12];
-
-fs= 300000;
-Vo = 16;
-
+close all
+Vd = 5:1:12
+fs= 300000; %300kHz
+Vo = 16; %V
+Po = 16; %W
 %Ilb =( Vo .* (1-D).*D )/(2*L*fs);
-
 %Iob =( Vo * (1-D).*(1-D).*D )/(2*L*fs);
 
 d = 1 - Vd./Vo
 
-
-Ro = 16;
-Iob = 1;
-
-
-L =( Vo * (1-d).*(1-d).*d )/(2.*Iob.*fs)
+Ro = 16; %ohm
+Iob = 1; %A
 
 
+L =( Vo * (1-d).*(1-d).*d )/(2.*Iob.*fs);
+max(L)
+hold on
+plot(d,L);
+ylabel("inductance(L)")
+xlabel("Duty Cycle")
+grid minor
+hold off
 
-
-l = (0.074*Vo)/(8*fs)
-x = (0.074*Vo)/(8*fs*l)
-
-Iob =( Vo * (1-d).*(1-d).*d)/(2*0.493*(10^-6)*fs)
-%d = 0.333;
-%Iob =( Vo * (1-d).*(1-d).*d)/(2*0.493*(10^-6)*fs)
-
-
-%%b
+%% b
 %Vripple = (d.*Vo)/(fs*Ro*C);
-Vripple = 0.02;
-C = (d.*Vo)/(fs*Ro*Vripple)
-
+Vripple = 0.02*Vo
+C = (d.*Vo)/(fs*Ro*Vripple);
+figure
+hold on
+plot (d,C)
+xlabel("Duty Cycle")
+ylabel("Output Capacitance (F)")
+grid minor
 
